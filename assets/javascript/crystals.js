c178ce55fdd91f2e23fceab7308cf806f770eda7
Fernando Mendoza <fern.mendoza13@gmail.com>
2018-11-13 20:48:21 -0800

    added click function for stone 1, wrote conditional for 'excelsior' and 'end is near'

--- assets/javascript/crystals.js
+++ assets/javascript/crystals.js
@@ -1,45 +1,55 @@
+$(document).ready(function() {
+  var Random = Math.floor(Math.random() * 101 + 19);
 
-
-$( document ).ready(function() {
-    var Random = Math.floor(Math.random()*101+19)
-
-    $('#magicNumber').text(Random);
+  $("#magicNumber").text(Random);
 });
 
-    var stone1= Math.floor(Math.random()*11+1)
-    var stone2= Math.floor(Math.random()*11+1)
-    var stone3= Math.floor(Math.random()*11+1)
-    var stone4= Math.floor(Math.random()*11+1)
+var stone1 = Math.floor(Math.random() * 11 + 1);
+var stone2 = Math.floor(Math.random() * 11 + 1);
+var stone3 = Math.floor(Math.random() * 11 + 1);
+var stone4 = Math.floor(Math.random() * 11 + 1);
 
-    var totalScoreboard = 0;
-    var wins = 0;
-    var losses = 0;
+var totalScoreboard = 0;
+var wins = 0;
+var losses = 0;
 
-    $('#wins').text(wins);
-    $('#losses').text(losses);
+$("#wins").text(wins);
+$("#losses").text(losses);
 
-    function reset(){
-        Random=Math.floor(Math.random()*101+19);
-        console.log(Random)
-        $('#magicNumber').text(Random);
-        stone1= Math.floor(Math.random()*11+1);
-        stone2= Math.floor(Math.random()*11+1);
-        stone3= Math.floor(Math.random()*11+1);
-        stone4= Math.floor(Math.random()*11+1);
-        userTotal= 0;
-        $('#totalScoreboard').text(userTotal);
-        } 
-    
-        function excelsior(){
-            alert("Excelsior!");
-              wins++; 
-              $('#wins').text(wins);
-              reset();
-            }
+function reset() {
+  Random = Math.floor(Math.random() * 101 + 19);
+  console.log(Random);
+  $("#magicNumber").text(Random);
+  stone1 = Math.floor(Math.random() * 11 + 1);
+  stone2 = Math.floor(Math.random() * 11 + 1);
+  stone3 = Math.floor(Math.random() * 11 + 1);
+  stone4 = Math.floor(Math.random() * 11 + 1);
+  userTotal = 0;
+  $("#totalScoreboard").text(userTotal);
+}
 
-        function endIsNear() {
-            alert("THE END IS NEAR!");
-            losses++;
-            $('#losses').text(losses);
-            reset();
-        }
+function excelsior() {
+  alert("Excelsior!");
+  wins++;
+  $("#wins").text(wins);
+  reset();
+}
+
+function endIsNear() {
+  alert("THE END IS NEAR!");
+  losses++;
+  $("#losses").text(losses);
+  reset();
+}
+
+$(".stone1").on("click", function() {
+  currentScore = currentScore + stone1;
+  console.log("New Total= " + currentScore);
+  $(".totalScoreboard").text(currentScore);
+  //sets win/lose conditions
+  if (currentScore == Random) {
+    excelsior();
+  } else if (userTotal > Random) {
+    endIsNear();
+  }
+});
