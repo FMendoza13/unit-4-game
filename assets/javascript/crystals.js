f7ffb5bdafaca27d5fa54714de16c7597ea50fe7
Fernando Mendoza <fern.mendoza13@gmail.com>
2018-11-13 19:52:13 -0800

    worked on JS/jQuery to select magicNumber, then tried to append to html.  Then tried to set random numbers for each gem

--- assets/javascript/crystals.js
+++ assets/javascript/crystals.js
@@ -1,3 +1,26 @@
-$( document ).ready(function(){
-    var Random=Math.floor(Math.random()*101+19)
-  
+
+
+$( document ).ready(function() {
+    var Random = Math.floor(Math.random()*101+19)
+
+    $('#theMagicNumber').text(Random);
+});
+
+    var stone1= Math.floor(Math.random()*11+1)
+    var stone2= Math.floor(Math.random()*11+1)
+    var stone3= Math.floor(Math.random()*11+1)
+    var stone4= Math.floor(Math.random()*11+1)
+
+    var totalScoreboard = 0;
+    var wins = 0;
+    var losses = 0;
+
+    $('#wins').text(wins);
+    $('#losses').text(losses);
+
+    function reset(){
+        Random=Math.floor(Math.random()*101+19);
+        console.log(Random)
+        $('#randomNumber').text(Random);
+
+    }
