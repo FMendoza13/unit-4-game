4413ca109a3e31976e8c066b39588e405fd4b996
Fernando Mendoza <fern.mendoza13@gmail.com>
2018-11-13 20:09:13 -0800

    try to add function to reset game when needed

--- assets/javascript/crystals.js
+++ assets/javascript/crystals.js
@@ -3,7 +3,7 @@
 $( document ).ready(function() {
     var Random = Math.floor(Math.random()*101+19)
 
-    $('#theMagicNumber').text(Random);
+    $('#magicNumber').text(Random);
 });
 
     var stone1= Math.floor(Math.random()*11+1)
@@ -21,6 +21,12 @@ $( document ).ready(function() {
     function reset(){
         Random=Math.floor(Math.random()*101+19);
         console.log(Random)
-        $('#randomNumber').text(Random);
-
-    }
+        $('#magicNumber').text(Random);
+        stone1= Math.floor(Math.random()*11+1);
+        stone2= Math.floor(Math.random()*11+1);
+        stone3= Math.floor(Math.random()*11+1);
+        stone4= Math.floor(Math.random()*11+1);
+        userTotal= 0;
+        $('#totalScoreboard').text(userTotal);
+        } 
+    
